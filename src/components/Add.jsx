3bfa2a73bcfb5acbd88a9e9d32517d4f5d71c5ca
Abhay Kumar Srivastava <ms2364911@gmail.com>
2023-10-15 01:19:34 +0530

    home and add product comp added

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,9 +1,93 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Add = () => {
-  return (
-    <div>Add</div>
-  )
-}
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
 
-export default Add
+  const [category, setCategory] = useState("");
+  const [brand, setBrand] = useState("");
+  const [error,setError] = useState(false);
+  const addProduct = async () => {
+    if(!name || !price || !category || !brand){
+      setError(true);
+      return false;
+    }
+    console.log(name, price, category, brand);
+    let userData = JSON.parse(localStorage.getItem("data"));
+    const userId = userData._id;
+
+    const result = await fetch("http://localhost:5000/add-product", {
+      method: "post",
+      body: JSON.stringify({ name, price, category, userId, brand }),
+      headers:{
+        "Content-Type":'application/json'
+      }
+    });
+    console.log(result)
+    
+  };
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        gap: "2vh",
+      }}
+    >
+      <h1>Add product</h1>
+      <input
+        style={{ width: "50vh", height: "5vh", paddingLeft: "5px" }}
+        type="text"
+        placeholder="enter product name"
+        onChange={(e) => setName(e.target.value)}
+      />
+      {
+        !name && error?      <span>please enter product name</span>:null
+      }
+      <input
+        style={{ width: "50vh", height: "5vh", paddingLeft: "5px" }}
+        type="text"
+        placeholder="enter price"
+        onChange={(e) => setPrice(e.target.value)}
+      />
+       {
+        !price && error?      <span>please enter price</span>:null
+      }
+      <input
+        style={{ width: "50vh", height: "5vh", paddingLeft: "5px" }}
+        type="text"
+        placeholder="enter category"
+        onChange={(e) => setCategory(e.target.value)}
+      />
+       {
+        !category && error?      <span>please enter  category</span>:null
+      }
+      
+      <input
+        style={{ width: "50vh", height: "5vh", paddingLeft: "5px" }}
+        type="text"
+        placeholder="enter product company"
+        onChange={(e) => setBrand(e.target.value)}
+      />
+       {
+        !brand && error?      <span>please enter brand</span>:null
+      }
+      <button
+        type="button"
+        style={{
+          background: "orange",
+          border: "none",
+          padding: "2vh",
+          color: "white",
+          cursor: "pointer",
+        }}
+        onClick={addProduct}
+      >
+        Add Product
+      </button>
+    </div>
+  );
+};
+
+export default Add;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [product, setProduct] = useState([]);
+  useEffect(() => {
+    getProduct();
+  }, []);
+  async function getProduct() {
+    let data = await fetch("http://localhost:5000/products");
+    let res = await data.json();
+    setProduct(res);
+  }
 
-export default Home
+  return (
+    <div style={{display:'flex', justifyContent:'center', marginTop:"5%", flexDirection:'column', alignItems:'center', gap:'2vh'}}>
+      <h1>Product List</h1>
+      <table>
+        <tbody>
+          <tr>
+            <th>S.No</th>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Category</th>
+            <th>brand</th>
+          </tr>
+           {
+            product.map((item,index)=>{
+              return <tr key={item._id}>
+                <td>{index+1}</td>
+                <td>{item.name}</td>
+                <td>{item.price}</td>
+                <td>{item.category}</td>
+                <td>{item.brand}</td>
+              </tr>
+            })
+           }
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Home;
